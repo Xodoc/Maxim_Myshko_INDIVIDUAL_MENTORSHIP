ad403dfe698a0a4287b75b8882f8e66bbfde4153
Maxim Myshko <mmyshko@exadel.com>
2022-01-31 19:38:33 +0300

    Add WeatherService test

---
+++ ConsoleApp/Tests/Services/WeatherServiceTest.cs
@@ -0,0 +1,40 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using BL.DTOs;
+using BL.Interfaces;
+using Moq;
+using Xunit;
+
+namespace Tests.Services
+{
+    public class WeatherServiceTest
+    {
+        private readonly Mock<IWeatherService> _weatherServiceMock;
+        private readonly IWeatherService _weatherService;
+        private readonly Fixture _fixture;
+
+        public WeatherServiceTest()
+        {
+            _fixture = (Fixture)new Fixture().Customize(new AutoMoqCustomization());
+            _weatherServiceMock = _fixture.Freeze<Mock<IWeatherService>>();
+            _weatherService = _fixture.Create<IWeatherService>();
+        }
+
+        [Fact]
+        public async void GetWeatherAsync_WhenSendingCorrectCityName_GettingWeather() 
+        {
+            //Arrange                        
+            var cityName = _fixture.Create<string>(); 
+            var expected = _fixture.Create<WeatherDTO>();            
+            
+            _weatherServiceMock.Setup(x => x.GetWeatherAsync(It.IsAny<string>()))
+                .ReturnsAsync(expected);
+
+            //Act
+            var actualWeatherDTO = await _weatherService.GetWeatherAsync(cityName);
+            
+            //Assert
+            Assert.Equal(expected, actualWeatherDTO);
+        }
+    }
+}
